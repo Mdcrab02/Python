19cea041485cff68a8c5c5a3e3e2cd46f82dfbea
Mdcrab02 <Mike.D.Crabtree@gmail.com>
2015-09-17 13:06:22 -0400

    Mean function
    Added the mean function, GitHub doesn't show each save as an uncommitted
    change.

---
+++ Scripts/Norm - StDev.py
@@ -0,0 +1,11 @@
+def Mean(l):
+    n = 0
+    total = 0.0
+    for i in l:
+        total = total + i
+        n = n+1
+    return total/n
+
+print Mean([1,2,3,4,5,6])
+
+#Let's add a test comment to see what GitHub does...
